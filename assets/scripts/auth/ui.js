7d009e5304454ddcf50d44d87b003b711d0e6b19
RobelsiTejada <robelsitejada@gmail.com>
2017-09-11 00:25:33 -0400

    Updating files

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,24 +4,19 @@ const app = require('../app.js')
 
 const signInSuccess = (data) => {
   app.user = data.user
-  console.log('sign in success!')
 }
 
 const signOutSuccess = () => {
   app.user = null
-  console.log('signed out')
 }
 
 const changePasswordSuccess = () => {
-  console.log('Password Successfully Changed.')
 }
 
 const signUpSuccess = (data) => {
-  console.log(data)
 }
 
 const success = (data) => {
-  console.log(data)
 }
 
 const failure = (error) => {
